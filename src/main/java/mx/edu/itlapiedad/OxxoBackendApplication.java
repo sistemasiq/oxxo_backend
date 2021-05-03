ea81bce4c569b898ee0da0af1590191ddf134816
Heriberto Flores <hflores@sistemasiq.com>
2021-05-03 10:22:20 -0500

    Commit inicial

---
+++ src/main/java/mx/edu/itlapiedad/OxxoBackendApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.itlapiedad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxxoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxxoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/edu/itlapiedad/OxxoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.itlapiedad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OxxoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
